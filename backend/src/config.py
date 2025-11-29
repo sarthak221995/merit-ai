4301b80bb7e82838dad48297b6015d7db9c6217b
Sarthak Dargan <sarthakdargan@Sarthaks-MacBook-Air.local>
2025-11-29 22:29:23 +0530

    Added Render Yaml

--- backend/src/config.py
+++ backend/src/config.py
@@ -5,7 +5,7 @@ class Settings(BaseSettings):
     APP_NAME: str = "ResumeForge"
     VERSION: str = "1.0.0"
     DEBUG: bool = True
-    CORS_ORIGINS: List[str] = ["http://localhost", "http://localhost:3000", "https://resumegpt-frontend.vercel.app"]
+    CORS_ORIGINS: List[str] = ["http://localhost", "http://localhost:3000", "https://resumegpt-frontend.vercel.app","https://merit-ai-zeta.vercel.app"]
 
     # OPENAI SETTINGS
     OPENAI_API_KEY: str
